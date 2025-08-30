3f6b4a0bec8d06de9417935e8c40b86d41018351
dongkyunKim <kyun010713@gmail.com>
2025-08-30 12:31:28 +0900

    chore: 스프링부트 프로젝트 생성

---
+++ src/main/java/org/carefreepass/com/carefreepassserver/CareFreePassServerApplication.java
@@ -0,0 +1,13 @@
+package org.carefreepass.com.carefreepassserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareFreePassServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareFreePassServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/carefreepass/com/carefreepassserver/CareFreePassServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carefreepass.com.carefreepassserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareFreePassServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
