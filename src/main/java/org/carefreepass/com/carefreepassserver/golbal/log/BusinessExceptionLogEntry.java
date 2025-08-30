22d307c3232f1247c9f48715f6b39a9919b1c945
dongkyunKim <kyun010713@gmail.com>
2025-08-30 14:02:49 +0900

    chore(#1): http 응답 템플릿 및 에러 템플릿 설정

---
+++ src/main/java/org/carefreepass/com/carefreepassserver/golbal/log/BusinessExceptionLogEntry.java
@@ -0,0 +1,35 @@
+package org.carefreepass.com.carefreepassserver.golbal.log;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class BusinessExceptionLogEntry {
+
+    private String timestamp;
+    private String exceptionType;
+    private String message;
+    private String requestUri;
+    private String stackTrace;
+
+    public String toLogString() {
+        return String.format("""
+            
+            [BUSINESS-EXCEPTION]
+              Time          : %s
+              ExceptionType : %s
+              Message       : %s
+              Request URI   : %s
+            -------------------- Stack Trace -------------------
+            %s
+            ----------------------------------------------------
+            """,
+                timestamp,
+                exceptionType,
+                message,
+                requestUri,
+                stackTrace
+        );
+    }
+}
